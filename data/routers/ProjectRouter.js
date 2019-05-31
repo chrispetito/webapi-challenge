789689af977a10e3ce51abbdc40afb31c47482c6
chrispetito <christopherpetito@gmail.com>
2019-05-31 08:45:44 -0700

    add CRUD calls to project router

--- data/routers/ProjectRouter.js
+++ data/routers/ProjectRouter.js
@@ -7,4 +7,23 @@ router.get('/', (req, res) => {
     db.get().then(projects => res.status(201).json(projects))
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    db.get(id).then(project => res.status(201).json(project))
+})
+
+router.post('/', (req, res) => {
+    db.insert(req.body).then(project => res.status(201).json(project))
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    db.update(id, req.body).then(project => res.status(201).json(project))
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    db.remove(id).then(() => res.status(201).json({ message: 'Project was successfully removed.' }))
+})
+
 module.exports = router;
