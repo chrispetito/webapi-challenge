2da654f44ddc0f6d65a7aa9fa00713a6daff7ccb
chrispetito <christopherpetito@gmail.com>
2019-05-31 09:08:28 -0700

    add 500 error messages to action and project routers; format docs

--- data/routers/ActionRouter.js
+++ data/routers/ActionRouter.js
@@ -1,29 +1,64 @@
-const express = require('express');
-const db = require('../helpers/actionModel');
+const express = require("express");
+const db = require("../helpers/actionModel");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    db.get().then(actions => res.status(201).json(actions))
-})
+//get all actions
+router.get("/", (req, res) => {
+  db.get()
+    .then(actions => res.status(201).json(actions))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: "There was an error retrieving the actions." })
+    );
+});
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    db.get(id).then(action => res.status(201).json(action))
-})
+//get action by id
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db.get(id)
+    .then(action => res.status(201).json(action))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: "There was an error retrieving the action." })
+    );
+});
 
-router.post('/', (req, res) => {
-    db.insert(req.body).then(action => res.status(201).json(action))
-})
+//add new action
+router.post("/", (req, res) => {
+  db.insert(req.body)
+    .then(action => res.status(201).json(action))
+    .catch(err =>
+      res.status(500).json({ message: "There was an error adding the action." })
+    );
+});
 
-router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    db.update(id, req.body).then(action => res.status(201).json(action))
-})
+//modify action by id
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  db.update(id, req.body)
+    .then(action => res.status(201).json(action))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: "There was an error modifying the action." })
+    );
+});
 
-router.delete('/:id', (req, res) => {
-    const { id } = req.params
-    db.remove(id).then(() => res.status(201).json({ message: 'Action has been successfully removed' }))
-})
+//delete action by id
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(() =>
+      res.status(201).json({ message: "Action has been successfully removed" })
+    )
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: "There was an error deleting the action." })
+    );
+});
 
 module.exports = router;
--- data/routers/ProjectRouter.js
+++ data/routers/ProjectRouter.js
@@ -1,29 +1,79 @@
-const express = require('express');
-const db = require('../helpers/projectModel');
+const express = require("express");
+const db = require("../helpers/projectModel");
+const actiondb = require("../helpers/actionModel");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    db.get().then(projects => res.status(201).json(projects))
-})
+//get all projects
+router.get("/", (req, res) => {
+  db.get()
+    .then(projects => res.status(201).json(projects))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: "There was an error retrieving the projects." })
+    );
+});
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params
-    db.get(id).then(project => res.status(201).json(project))
-})
+//get project by id
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db.get(id)
+    .then(project => res.status(201).json(project))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: "There was an error retrieving this project." })
+    );
+});
 
-router.post('/', (req, res) => {
-    db.insert(req.body).then(project => res.status(201).json(project))
-})
+//get project actions by id
+router.get("/:id/actions", (req, res) => {
+  const { id } = req.params;
+  db.getProjectActions(id)
+    .then(actions => res.status(201).json(actions))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: "There was an error retrieving the project actions." })
+    );
+});
 
-router.put('/:id', (req, res) => {
-    const { id } = req.params
-    db.update(id, req.body).then(project => res.status(201).json(project))
-})
+//add new project
+router.post("/", (req, res) => {
+  db.insert(req.body)
+    .then(project => res.status(201).json(project))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: "There was an error adding the project." })
+    );
+});
 
-router.delete('/:id', (req, res) => {
-    const { id } = req.params
-    db.remove(id).then(() => res.status(201).json({ message: 'Project was successfully removed.' }))
-})
+//modify project by id
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  db.update(id, req.body)
+    .then(project => res.status(201).json(project))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: "There was an error modifying the project." })
+    );
+});
+
+//delete project by id
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(() =>
+      res.status(201).json({ message: "Project was successfully removed." })
+    )
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: "There was an error deleting the project." })
+    );
+});
 
 module.exports = router;
